e18be3667ff3e64b532cbc8ac37685f4e0804ad2
GYTFree <372763746@qq.com>
2018-09-18 19:22:40 +0800

    web pageinator nav

--- boring/urls.py
+++ boring/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('crawler.urls', 'crawler'), namespace='crawler')),
+    path('', include('crawler.urls')),
     path('crawler/', include(('crawler.urls', 'crawler'), namespace='crawler')),
 ]
