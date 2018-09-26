b99e1e84f57f2055f101e5b4cd9eaa7c7a35d84f
GYTFree <372763746@qq.com>
2018-09-26 15:50:27 +0800

    set static_root

--- boring/settings.py
+++ boring/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dk5$504+&w8u&=@dxfl^*yl@!yl6gq2-=$i-_h&+u&m%4=m35^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -133,4 +133,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = '/data/apps/webapps/boring/static'
+# STATIC_ROOT = '/data/apps/webapps/boring/static'
